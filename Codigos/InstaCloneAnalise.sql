b0204df398375673e65ee38abfda7af4f7d06f00
Thays Ferreira <thaysferreiraua@gmail.com>
2023-01-06 20:12:17 -0300

    Analise dos dados

--- Codigos/InstaCloneAnalise.sql
+++ Codigos/InstaCloneAnalise.sql
@@ -35,9 +35,44 @@ SELECT COUNT(DISTINCT(usuarios.id)) AS usuariosComPosts FROM usuarios
     JOIN fotos ON usuarios.id = fotos.usuarioId;
 
 
-/*A brand wants to know which hashtags to use in a post
-What are the top 5 most commonly used hashtags?*/
+/*Encontre as 5 hashtags mais utilizadas pelos usuarios*/
 SELECT nomeTag, COUNT(nomeTag) AS total FROM tags
     JOIN tagsFotos ON tags.id = tagsFotos.tagId
     GROUP BY tags.id ORDER BY total DESC;
+    
 
+/*Encontre os usuarios que curtiram todas as fotos do site*/
+SELECT usuarios.id, usuario, COUNT(usuarios.id) As nCurtidas FROM usuarios
+    JOIN curtidas ON usuarios.id = curtidas.usuarioId GROUP BY usuarios.id
+    HAVING nCurtidas = (SELECT COUNT(*) FROM fotos);
+    
+    
+/*Encontre os usuarios que nunca comentaram em nenhuma postagem*/
+SELECT usuario FROM usuarios
+    LEFT JOIN comentarios ON usuarios.id = comentarios.usuarioId
+    WHERE comentarios.comentario IS NULL;
+
+
+/*Encontre os usuarios que já comentaram em alguma postagem ao menos uma vez*/
+SELECT usuario FROM usuarios
+    JOIN comentarios ON usuarios.id = comentarios.usuarioId
+    GROUP BY usuario;
+
+
+/*Encontre a porcentagem de usuarios que já comentaram em alguma postagem e a de
+usuarios que nunca comentaram*/
+CREATE VIEW NuncaComentaram AS 
+    SELECT usuario FROM usuarios
+        LEFT JOIN comentarios ON usuarios.id = comentarios.usuarioId
+        WHERE comentarios.comentario IS NULL;
+
+CREATE VIEW JaComentaram AS
+    SELECT usuario FROM usuarios
+        JOIN comentarios ON usuarios.id = comentarios.usuarioId
+        GROUP BY usuario;
+
+SELECT 
+	CONCAT(ROUND((NuncaComentaram.n/(SELECT COUNT(*) FROM usuarios))*100), '%') AS 'Nunca Comentaram',
+	CONCAT(ROUND((Comentaram.n/(SELECT COUNT(*) FROM usuarios))*100), '%') AS 'Comentaram'
+FROM(SELECT(SELECT COUNT(*) FROM NuncaComentaram) AS n) AS NuncaComentaram
+JOIN(SELECT(SELECT COUNT(*) FROM JaComentaram) AS n) AS Comentaram;
